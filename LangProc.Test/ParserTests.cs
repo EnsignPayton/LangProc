832585025c10f394699ef0644daa3c7d37b547db
Logan Payton <Logan.Payton@javs.com>
2018-06-06 23:19:11 -0400

    Parser basic test

---
+++ LangProc.Test/ParserTests.cs
@@ -0,0 +1,27 @@
+﻿using LangProc.Core;
+using NUnit.Framework;
+
+namespace LangProc.Test
+{
+    public class ParserTests
+    {
+        [Test]
+        public void Parser_Basic()
+        {
+            var tokens = new[]
+            {
+                new Token(TokenType.Integer, 1),
+                new Token(TokenType.Add),
+                new Token(TokenType.Integer, 1),
+                new Token(TokenType.EndOfFile)
+            };
+
+            using (var parser = new Parser(tokens))
+            {
+                var result = parser.Parse();
+
+                Assert.AreEqual(result, 2);
+            }
+        }
+    }
+}
